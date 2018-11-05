28151f45ff0cfb79258ccb62ea33378576ca6ba8
Thinker Pal <thinkerpal@gmail.com>
2018-11-05 14:53:50 +0800

    Initial Commit

---
+++ Screenshots/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screenshots
+//
+//  Created by Rui Yang Tan on 5/11/18.
+//  Copyright © 2018 Rui Yang Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
